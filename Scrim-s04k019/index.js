5886ca406135b66284500902b0a02854fe6acc6b
ganeshxwz <ganeshxwz@gmail.com>
2025-10-19 04:20:31 +0530

    initial commit

---
+++ Scrim-s04k019/index.js
@@ -0,0 +1,38 @@
+ 
+
+let homeCount = document.getElementById("home-count")
+let guestCount = document.getElementById("guest-count")
+ 
+count = 0
+
+function add1(){
+    count += 1
+    homeCount.textContent = count
+}
+  
+function add2(){
+    count += 2
+    homeCount.textContent = count
+}
+
+function add3(){
+    count += 3
+    homeCount.textContent = count
+}
+
+count2 = 0
+
+function addg1(){
+    count2 += 1
+    guestCount.textContent = count2
+}
+
+function addg2(){
+    count2 += 2
+    guestCount.textContent = count2
+}
+
+function addg3(){
+    count2 += 3
+    guestCount.textContent = count2
+}
